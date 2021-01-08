9a575e54d9d4f6b083b104df1adceefbd795531e
Jefferson <drankhar@gmail.com>
2021-01-07 23:27:10 -0300

    Commit inicial do projeto, configuração inicial do projeto configurada, tela de login implementada, conexão com banco de dados implementada. Classe pessoa implementada.

---
+++ Helloworld/src/main/java/br/com/offtechnology/Helloworld/entidade/Pessoa.java
@@ -0,0 +1,72 @@
+package br.com.offtechnology.Helloworld.entidade;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // Transforma a classe em tabela no banco de dados
+public class Pessoa implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id // Gera a primary Key
+	@GeneratedValue(strategy = GenerationType.AUTO) // Gera o sequênciador automatico
+	private Long id;
+	
+	@Column(unique = true)
+	private String login;
+	
+	private String password;
+
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
